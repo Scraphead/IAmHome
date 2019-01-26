76ff6b7d1d1ce2957f40e6d2496ceb5d5e338e6a
Riskoker <kwanmui5@gmail.com>
2019-01-26 17:10:45 +0100

    hand

---
+++ IAmHome/Assets/Scripts/HandHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandHandler : MonoBehaviour
+{
+    public int playerId = 0;
+
+    public float moveSpeed = 3.0f;
+   
+  
+
+    private CharacterController cc;
+    private Vector3 moveVector;
+    
+    
+    void OnEnable()
+    {
+      
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        
+
+        GetInput();
+        ProcessInput();
+    }
+
+    private void GetInput()
+    {
+      
+
+        //moveVector.x = Input.GetKeyDown(KeyCode.L); 
+        //moveVector.y = GetAxis("MoveHandVertical");
+     
+    }
+
+    private void ProcessInput()
+    {
+      
+        if (moveVector.x != 0.0f || moveVector.y != 0.0f)
+        {
+            cc.Move(moveVector * moveSpeed * Time.deltaTime);
+        }
+
+       
+    }
+}
+
