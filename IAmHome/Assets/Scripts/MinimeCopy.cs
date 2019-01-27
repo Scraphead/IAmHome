b02cb34e29b3218d826073dce831e706ca3e52cc
Henrik Goldsack <henrik0312@gmail.com>
2019-01-27 09:56:05 +0100

    Add minime

---
+++ IAmHome/Assets/Scripts/MinimeCopy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimeCopy : MonoBehaviour
+{
+    public GameObject bodyFrom;
+    public GameObject bodyTo;
+    public GameObject headFrom;
+    public GameObject headTo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bodyTo.transform.localPosition = bodyFrom.transform.position;
+        bodyTo.transform.localRotation = bodyFrom.transform.rotation; 
+        
+        headTo.transform.localPosition = headFrom.transform.position;
+        headTo.transform.localRotation = headFrom.transform.rotation;
+    }
+}
