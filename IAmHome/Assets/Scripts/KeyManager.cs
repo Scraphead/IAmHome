cfe0a51af52e9644c44a5d87270c242cd24d99c8
Henrik Goldsack <henrik0312@gmail.com>
2019-01-27 10:42:36 +0100

    Fixed hand scale

---
+++ IAmHome/Assets/Scripts/KeyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManager : MonoBehaviour
+{
+
+    public static KeyManager instance;
+
+    public bool KeyOnDesk;
+    public bool KeyOnHand;
+    public bool KeyOverHead;
+    public bool KeyInDoor;
+    
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+}
