21faeb4ed8d5baa3e285a42445c44d63963ceebc
Riskoker <kwanmui5@gmail.com>
2019-01-27 01:45:06 +0100

    shiet

---
+++ IAmHome/Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class DoorHandler : MonoBehaviour
+{
+    public UnityEvent onOpenDoorEvent;
+    // Start is called before the first frame update
+   
+    public void OnOpenDoor()
+    {
+        if (!GameManager.Instance.haveKey)
+            return;
+        Debug.Log("Door OPENED");
+        GameManager.Instance.openedDoor = true;
+        if (onOpenDoorEvent != null)
+            onOpenDoorEvent.Invoke();
+        GameManager.Instance.GameFinnishhed();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Door interacted with player, but no key");
+        }
+    }
+
+}
